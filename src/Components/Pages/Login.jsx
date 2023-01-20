1ecd9b41722009f4d2454bd42702194d70091ea8
TartCodes <tartcodes@gmail.com>
2023-01-20 12:56:16 -0500

    need to come back later

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -1,9 +1,11 @@
+import classes from "./Login.module.css";
+import Button from "../UI/Button";
+
 const Login = (props) => {
-	console.log(props.login);
 	return (
-		<div className="main-wrapper">
-			<div className="login">Login</div>
-			<button onClick={props.login}>Click</button>
+		<div className={classes.login}>
+			<div className="">Login</div>
+			<Button onClick={props.login}>Login</Button>
 		</div>
 	);
 };
---
+++ src/Components/UI/Card.jsx
@@ -0,0 +1,1 @@
+//used to wrap some components
